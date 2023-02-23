8c1d69d5f5d60d046580b46dccbda3269be1fdce
almirrps <almirrps@hotmail.com>
2023-02-23 12:29:22 -0300

    Criando o projeto Planets Star Wars

---
+++ src/main/java/com/udemy/planets/PlanetsStarWarsApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.planets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetsStarWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetsStarWarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/planets/PlanetsStarWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.planets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetsStarWarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
