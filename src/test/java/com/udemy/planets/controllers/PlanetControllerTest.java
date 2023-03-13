4feae389f35e9a4c3278090466b81c298d0a88a1
Almir <almirrps@hotmail.com>
2023-03-13 13:16:24 -0300

    Controller: Testando dados criados

---
+++ src/test/java/com/udemy/planets/controllers/PlanetControllerTest.java
@@ -0,0 +1,39 @@
+package com.udemy.planets.controllers;
+
+import static com.udemy.planets.commons.PlanetConstants.PLANET_MODEL;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.when;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.udemy.planets.services.PlanetService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(PlanetController.class)
+public class PlanetControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private PlanetService planetService;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsCreated() throws Exception {
+        when(planetService.create(PLANET_MODEL)).thenReturn(PLANET_MODEL);
+
+        mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(PLANET_MODEL)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$").value(PLANET_MODEL)); // Validando a propriedade raiz do json
+    }
+
+}
