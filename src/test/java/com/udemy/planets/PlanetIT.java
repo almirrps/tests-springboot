7d245329fdadd103d6ef1edb7621e7c807a590a0
Almir <almirrps@hotmail.com>
2023-03-13 23:53:59 -0300

    Profiles de teste

--- src/test/java/com/udemy/planets/PlanetIT.java
+++ src/test/java/com/udemy/planets/PlanetIT.java
@@ -1,9 +1,11 @@
-package com.udemy.planets.controllers;
+package com.udemy.planets;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)  // Subindo o BD com TomCat em porta aleatória
+@ActiveProfiles("it")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)  // Subindo o BD MySQL com TomCat em porta aleatória
 public class PlanetIT {
 
     // O método abaixo verifica se o contexto da aplicação foi carregado com sucesso
